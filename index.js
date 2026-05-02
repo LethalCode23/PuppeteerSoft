ffe5b9de6997b651fcffa55e0d53fcf9c541f029
Cristian Diaz <xanandramago12@gmail.com>
2026-05-02 10:59:57 -0500

    Fix for render Deploy

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const { generateStickerImage } = require('./routes/sticker');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.get('/sticker', async (req, res) => {
+  
   try {
     const buffer = await generateStickerImage(req.query);
     res.set('Content-Type', 'image/png');
     res.send(buffer);
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Error generando el sticker' });
   }
 });
 
-app.listen(3000, () => {
-  console.log('Servidor en http://localhost:3000');
+app.listen(PORT, () => {
+  console.log(`Servidor en puerto ${PORT}`);
 });
